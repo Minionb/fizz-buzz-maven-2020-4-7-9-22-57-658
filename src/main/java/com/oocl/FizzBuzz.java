df4b6d8a9e5929b9944e477ea1574b1202858c18
HILARY NG (INFRA-ISD-OOCLL/HKG) <NGHI2@oocl.com>
2020-04-08 16:17:34 +0800

    [Hilary]: Done return Fizz method

--- src/main/java/com/oocl/FizzBuzz.java
+++ src/main/java/com/oocl/FizzBuzz.java
@@ -2,6 +2,11 @@ package com.oocl;
 
 public class FizzBuzz {
     public String say(int Number){
+
+        if(Number % 3 == 0){
+            return "Fizz";
+        }
+
         return String.valueOf(Number);
     }
 }
